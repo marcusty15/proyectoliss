f695aaef9f132e059fb9f833cff1a39eea732b6a
marcusty15 <marco.troconis15@gmail.com>
2022-09-20 18:13:15 -0400

    test

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
   return (
     <>
    
-     <Router>
+     <Router basename={process.env.PUBLIC_URL}>
         <Routes>
           <Route exact path="/" element={<Home />} />
          
