3e5ff51680f2e00324c6159304963e95fdd5f9ac
marcusty15 <marco.troconis15@gmail.com>
2022-10-08 09:43:02 -0400

    se ajustaron medidas para diferentes pantallas

--- src/Components/Video/Video.js
+++ src/Components/Video/Video.js
@@ -4,7 +4,7 @@ import './Video.css'
 const Video = () => {
   return (
     <div className='vid'>
-        
+        <iframe className='video'  src="https://www.youtube.com/embed/Fh7kMPx4Exc" title="Encuentro: ¿Cómo transformar el apego en libertad?" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
     </div>
   )
 }
